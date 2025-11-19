8903e1e438c6565ac972ed66326ff66cd609bbd6
Yaël Morejon Marquez <y.morejonmarquez@outlook.com>
2025-11-19 12:29:48 +0100

    - Started work on the implementation of the yellow category. - Added a tests folder and tests for yellow.py - Lowercased the clever folder from "Clever" to "clever - Added __init__.py files to the "clever" and "tests" folder for testing purposes

---
+++ clever/yellow.py
@@ -0,0 +1,47 @@
+'''
+The yellow category is a 4x4 grid. Points are gained by crossing columns,
+Bonuses are gained by crossing rows, and via the diagonal from top left to
+bottom right. 4 boxes are already crossed. A box may be crossed when you match
+the number.
+'''
+
+
+grid = [
+    [3, 6, 5, 'x'],
+    [2, 1, 'x', 5],
+    [1, 'x', 2, 4],
+    ['x', 3, 4, 6],
+    ]
+
+points = [10, 14, 16, 20]
+
+bonuses = ["cross_blue", "four_orange", "cross_green", "fox", "+1"]
+
+
+class Yellow:
+
+    def __init__(self, grid=grid, points=points, bonuses=bonuses):
+
+        self.grid = grid
+        self.points = points
+        self.bonuses = bonuses
+        pass
+
+    def coordinates(self, value):
+
+        coordinates = []
+
+        for row, val in enumerate(self.grid):
+            for col, val in enumerate(self.grid[row]):
+
+                if val == value:
+                    coordinates.append([row, col])
+
+        return coordinates
+
+    def cross(self, value, occurrence):
+
+        coordinates = self.coordinates(value)
+        row = coordinates[occurrence][0]
+        column = coordinates[occurrence][1]
+        self.grid[row][column] = 'x'
