d9515119eae69496742521ea6e7f26c2fbb5b6be
Yaël Morejon Marquez <y.morejonmarquez@outlook.com>
2025-12-03 12:10:45 +0100

    Implemented dice and tests. Change .gitignore

--- clever/dice.py
+++ clever/dice.py
@@ -1,5 +1,28 @@
-import random as rd
+'''
+Clever is played with six dice, one for each category and a white die that acts
+as a "joker" die. The category dice can only filled in their matching category
+and the white die can be used everywhere.
+'''
+
+import random
 
 
-def roll_dice():
-    return rd.randint(1, 2)
+class Die():
+
+    def __init__(self):
+
+        self.value = 0
+        self.on_platter = False
+
+    def roll(self):
+
+        self.value = random.randint(1, 6)
+        return self.value
+
+    def update_state(self, change):
+
+        if change == "hand":
+            self.on_platter = False
+
+        elif change == "platter":
+            self.on_platter = True
